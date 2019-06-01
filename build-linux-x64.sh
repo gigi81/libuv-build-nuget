7fedc989ce82d1866333bd2dff88c978d67b09f4
Luigi Grilli <luigi.grilli@gmail.com>
2019-06-01 23:14:49 +0100

    Fixes for linux/osx scripts

--- build-linux-x64.sh
+++ build-linux-x64.sh
@@ -4,4 +4,5 @@ python --version
 
 cd libuv
 ../gyp/gyp -I common.gypi test/test.gyp -f make --depth=. -Duv_library=shared_library -Dtarget_arch=x64
-BUILDTYPE=Release make -C out
+BUILDTYPE=Release
+make
--- build-osx-x64.sh
+++ build-osx-x64.sh
@@ -4,4 +4,6 @@ python --version
 
 cd libuv
 ../gyp/gyp -I common.gypi test/test.gyp -f xcode --depth=. -Duv_library=shared_library
-xcodebuild ARCHS="x86_64" -project uv.xcodeproj -configuration Release -target All
+find *.xcodeproj
+
+xcodebuild ARCHS="x86_64" -project uv.xcodeproj -configuration Release -alltargets
