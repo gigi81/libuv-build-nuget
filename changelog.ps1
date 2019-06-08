89640fa9a6057ed1de33d30729fa6ff118f967b3
Luigi Grilli <luigi.grilli@gmail.com>
2019-06-08 11:48:29 +0100

    Fixed license, added changelog

---
+++ changelog.ps1
@@ -0,0 +1,29 @@
+﻿$first = $true
+$changeLog = ""
+
+foreach($line in Get-Content -Path "$PSScriptRoot\libuv\ChangeLog")
+{
+    if($line -match '\d{4}\.\d{2}\.\d{2}.*')
+    {
+        if($first)
+        {
+            $first = $false
+        }
+        else
+        {
+            break
+        }
+    }
+
+    if(!$first -and ![String]::IsNullOrWhiteSpace($line))
+    {
+        $changeLog += $line
+        $changeLog += [System.Environment]::NewLine
+    }
+}
+
+$changeLog
+
+[xml]$xml = Get-Content "$PSScriptRoot\libuv-shared.nuspec"
+$xml.package.metadata.releaseNotes = $changeLog
+$xml.Save("$PSScriptRoot\libuv-shared.nuspec")
