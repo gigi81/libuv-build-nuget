714e8b3289f4a196feb166f8f9fbbf4d057b7562
Luigi Grilli <luigi.grilli@gmail.com>
2019-06-16 12:41:56 +0100

    Workaround for packageVersion not having prerelease. Inlined ps script

--- changelog.ps1
+++ changelog.ps1
@@ -1,29 +1,23 @@
-﻿$first = $true
+﻿$first = $false
 $changeLog = ""
+$path = Join-Path (Join-Path (Get-Location) 'libuv') 'ChangeLog'
+$nuspec = Join-Path (Get-Location) 'libuv-shared.nuspec'
 
-foreach($line in Get-Content -Path "$PSScriptRoot\libuv\ChangeLog")
+foreach($line in Get-Content -Path $path)
 {
     if($line -match '\d{4}\.\d{2}\.\d{2}.*')
     {
-        if($first)
-        {
-            $first = $false
-        }
-        else
-        {
-            break
-        }
+        if($first) { break; }
+        $first = $true
     }
 
-    if(!$first -and ![String]::IsNullOrWhiteSpace($line))
+    if($first -and ![String]::IsNullOrWhiteSpace($line))
     {
         $changeLog += $line
         $changeLog += [System.Environment]::NewLine
     }
 }
 
-$changeLog
-
-[xml]$xml = Get-Content "$PSScriptRoot\libuv-shared.nuspec"
+[xml]$xml = Get-Content $nuspec
 $xml.package.metadata.releaseNotes = $changeLog
-$xml.Save("$PSScriptRoot\libuv-shared.nuspec")
+$xml.Save($nuspec)
